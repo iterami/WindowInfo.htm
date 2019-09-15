db1e78d8c55326463b093b3cef87a275153e225b
Jan Hořava <czechinthemail@gmail.com>
2019-09-15 13:48:12 -0700

    replaced depth span with button, prevented depthless redirect

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@
     if(Number.isNaN(depth)
       || depth < 0){
         depth = 0;
-        window.location.replace('?0');
     }
 
     let display = [];
@@ -57,6 +56,20 @@
         }
     }
 
-    document.getElementById('depth').innerHTML = depth;
+    document.getElementById('depth').value = 'Depth: ' + depth;
+    document.getElementById('depth').onclick = function(){
+        let newdepth = window.prompt(
+          'Enter new depth:',
+          depth
+        );
+
+        if(newdepth === null
+          || newdepth.length === 0
+          || isNaN(newdepth)){
+            return;
+        }
+
+        window.location.replace('?' + newdepth);
+    };
     document.getElementById('result').innerHTML = display.sort().join('');
 }());
