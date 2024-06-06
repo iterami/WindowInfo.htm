ca744ed172ac13d99fc345cd808ea4dee28b04cb
Jan Hořava <czechinthemail@gmail.com>
2024-06-06 02:29:48 -0700

    replaced Number.isNaN() with globalThis.isNaN()

--- repo.js
+++ repo.js
@@ -6,7 +6,7 @@
       10
     );
 
-    if(Number.isNaN(depth)
+    if(globalThis.isNaN(depth)
       || depth < 0){
         depth = 0;
     }
@@ -57,7 +57,7 @@
 
         if(newdepth === null
           || newdepth.length === 0
-          || Number.isNaN(newdepth)){
+          || globalThis.isNaN(newdepth)){
             return;
         }
 
